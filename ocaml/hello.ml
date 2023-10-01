bf9b3438a45481d097062394bd0171bdfd077abf
Axel Clark <axel.clark@pm.me>
2023-09-30 21:24:05 -0700

    Update hello and add counting_characters

--- ocaml/hello.ml
+++ ocaml/hello.ml
@@ -1,4 +1,7 @@
 let () =
   print_string "What is your name? ";
+
   let name = read_line () in
-  Printf.printf "Hello, %s, nice to meet you!\n" name
+  let message = Printf.sprintf "Hello, %s, nice to meet you!\n" name in
+
+  print_endline message
