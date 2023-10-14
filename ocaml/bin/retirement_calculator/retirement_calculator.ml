244eac5cc9dad5a178a4ec27c3397db9545dbd4e
Axel Clark <axel.clark@pm.me>
2023-10-14 09:14:35 -0700

    Simplify retirement_calculator

--- ocaml/bin/retirement_calculator/retirement_calculator.ml
+++ ocaml/bin/retirement_calculator/retirement_calculator.ml
@@ -19,19 +19,17 @@ let rec prompt_for_input prompt =
       prompt_for_input prompt
 
 let () =
-  let prompts =
-    [ "What is your current age?\n"; "What age would you like to retire?\n" ]
+  let age = prompt_for_input "What is your current age?\n" in
+  let retirement_age =
+    prompt_for_input "What age would you like to retire?\n"
   in
 
-  match List.map prompt_for_input prompts with
-  | [ age; retirement_age ] ->
-      if retirement_age > age then
-        let year = current_year () in
-        let years_left = retirement_age - age in
-        let retirement_year = year + years_left in
-        Printf.printf
-          "You have %i years left until you retire.\n\
-           It's %i, so you can retire in %i.\n"
-          years_left year retirement_year
-      else Printf.printf "You can already retire!\n"
-  | _ -> failwith "Unexpected number of responses"
+  if retirement_age > age then
+    let year = current_year () in
+    let years_left = retirement_age - age in
+    let retirement_year = year + years_left in
+    Printf.printf
+      "You have %i years left until you retire.\n\
+       It's %i, so you can retire in %i.\n"
+      years_left year retirement_year
+  else Printf.printf "You can already retire!\n"
