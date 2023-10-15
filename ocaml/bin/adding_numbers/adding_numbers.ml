7cae63288c32982eb9c5cdfec9fcb524434bc6f8
Axel Clark <axel.clark@pm.me>
2023-10-14 20:10:49 -0700

    Finish adding_numbers

--- ocaml/bin/adding_numbers/adding_numbers.ml
+++ ocaml/bin/adding_numbers/adding_numbers.ml
@@ -1,23 +1,28 @@
-let rec prompt_for_input prompt =
+let rec prompt_for_integer_input prompt =
   print_string prompt;
   let input = read_line () in
   match int_of_string_opt input with
   | Some int_input when int_input < 1 ->
       print_endline "Error: Please provide a number greater than or equal to 1.";
-      prompt_for_input prompt
+      prompt_for_integer_input prompt
   | Some int_input -> int_input
   | None ->
       if input = "" then
         print_endline "Error: Please provide a non-blank input."
       else print_endline "Error: Please provide a valid number.";
-      prompt_for_input prompt
+      prompt_for_integer_input prompt
 
-let range i = List.init i succ
+let call_prompt_n_times n =
+  let times = List.init n (fun _ -> "Enter a number:") in
+  List.map prompt_for_integer_input times
+
+let sum_numbers_in_list numbers = List.fold_left ( + ) 0 numbers
 
 let () =
-  let numbers_to_add = prompt_for_input "How many numbers should we add?" in
-  Printf.printf "We are going to add %i numbers." numbers_to_add;
-
-  let numbers = range 5 in
-
-  let number_inputs = List.map (fun _ -> 5 + 5;) [5; 5] in
+  let numbers_to_add =
+    prompt_for_integer_input "How many numbers should we add?"
+  in
+  Printf.printf "We are going to add %i numbers.\n" numbers_to_add;
+  let numbers = call_prompt_n_times numbers_to_add in
+  let sum = sum_numbers_in_list numbers in
+  Printf.printf "The total is %i.\n" sum
